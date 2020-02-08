5275f746d44db75fc044bfb0e6d66bf85bd58b15
ruczak-m <ruczak-m@webmail.uwinnipeg>
2020-02-07 22:16:59 -0600

    Resync Branch

---
+++ Speedy Red Ball/Assets/_Scripts/Collectable_Collision.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectable_Collision : MonoBehaviour
+{
+    //public Level_Info levelInfo;
+    public Text playerScore_Text;
+    public Text playerInstruction_Text;
+    private int playerScore;
+    private int maxScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Getting the highest score the player can get for this level
+        maxScore = Level_Info.getCurrLevelMaxScore();
+
+        //Checking if a message needs to be displayed
+        string levelMessage = Level_Info.getCurrLevelMessage();
+        if (levelMessage != null)
+        {
+            playerInstruction_Text.text = levelMessage;
+        }
+        else
+        {
+            playerInstruction_Text.text = "";    //Displaying nothing in the message area text
+        }
+
+        Debug.Log("Current Scene Name: " + Level_Info.getCurrentSceneName());
+        Debug.Log("The max score for this level is " + maxScore);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+     *  Handles when the collision between the player and a trigger
+     *
+    */ 
+    private void OnTriggerEnter(Collider other)
+    {
+        //Checking if the GameObject the Player Collided with was a collectable
+        if (other.gameObject.CompareTag("collectable"))
+        {
+            other.gameObject.SetActive(false);  //Hidding the object that the player came in contact
+            playerScore++;  //Incrementing the players score
+            updatePlayerScore();
+
+            //Checking if the player has reached the max score for this level
+            if (playerScore >= maxScore)
+            {
+                if (Level_Info.loadNextLevel() == -1)
+                {
+                    playerInstruction_Text.text = "You win";
+                    Player_Controls.isGameOver = true;  //This allows the player to keep moving around the final level
+                }
+                
+            }
+        }
+    }
+
+    //Updates the player Score in the Scene
+    private void updatePlayerScore()
+    {
+        playerScore_Text.text = "Score: " + playerScore.ToString();
+    }
+}
---
+++ Speedy Red Ball/Assets/_Scripts/Level_Info.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Info : MonoBehaviour
+{
+    private static int numberOfLevels = 3;
+    //Returns the name of the current scene
+    public  static string getCurrentSceneName()
+    {
+        return SceneManager.GetActiveScene().name;  //Returning the name of the current scene
+    }
+
+    //Returns the max number score for the current level
+    public static int getCurrLevelMaxScore()
+    {
+        //Return the max score depending on the current scene
+        switch (getCurrentSceneName())
+        {
+            //Create a new case when creating a new level
+            case "Level_1": return 3;
+            case "Level_2": return 4;
+            case "Level_3": return 17;
+            default: return -1;
+        }
+    }
+    public static string getCurrLevelMessage()
+    {
+        //Return the max score depending on the current scene
+        switch (getCurrentSceneName())
+        {
+            //Create a new case when creating a new level
+            case "Level_1": return "Controls: \n" +
+                    "W = Forward \n" +
+                    "S = Backward \n" +
+                    "A = Left \n" +
+                    "D = Right \n" +
+                    "C = Change Camera";
+            default: return null;
+        }
+    }
+
+    public static double getCurrLevelTimeToComplete()
+    {
+        //Return the max score depending on the current scene
+        switch (getCurrentSceneName())
+        {
+            //Create a new case when creating a new level
+            case "Level_1":
+                return 25.0;
+            case "Level_2":
+                return 15.0;
+            case "Level_3":
+                return 30.0;
+            default: return 30.0;
+        }
+    }
+
+    public static int loadNextLevel()
+    {
+        //Getting the next level's number
+        int nextLevelNum = System.Int32.Parse(getCurrentSceneName().Split('_')[1]) + 1;
+        //Checking if the next level exist (Checking if their is another level)
+        if (!(nextLevelNum > numberOfLevels))
+        {
+            SceneManager.LoadScene(sceneName: "Level_" + nextLevelNum);
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
